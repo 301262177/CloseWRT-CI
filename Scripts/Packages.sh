55b7555c3a6d5ea433050f672b9a4ec7290e10bb
al <66003967+301262177@users.noreply.github.com>
2024-11-25 20:16:32 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -24,6 +24,8 @@ UPDATE_PACKAGE() {
 UPDATE_PACKAGE "argon" "jerrykuku/luci-theme-argon" "master"
 UPDATE_PACKAGE "kucat" "sirpdboy/luci-theme-kucat" "js"
 
+UPDATE_PACKAGE "istore-ui" "linkease/istore-ui" "main" "pkg"
+UPDATE_PACKAGE "istore" "linkease/istore" "main" "pkg"
 UPDATE_PACKAGE "nekoclash" "Thaolga/luci-app-nekoclash" "main"
 UPDATE_PACKAGE "openclash" "vernesong/OpenClash" "dev" "pkg"
 UPDATE_PACKAGE "passwall" "xiaorouji/openwrt-passwall" "main" "pkg"
